bea76174e56a6edd24fe819db4a3f2652045782b
hiroaki.nishimori <hiroaki.nishimori@hra-inc.co.jp>
2023-11-23 15:53:29 +0900

    push zip

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,20 +35,20 @@ resource "aws_s3_bucket_lifecycle_configuration" "terraform" {
   }
 }
 
-# resource "aws_lambda_function" "lambda_function" {
-#   filename      = "${path.module}/lambda/lambda_function.zip"
-#   function_name = "notify_aws_cost"  # 任意の関数名に
-#   role          = aws_iam_role.lambda_exec_role.arn
-#   handler       = "index.handler"
-#   runtime       = "python3.10"
-#   source_code_hash = filebase64sha256("${path.module}/lambda/lambda_function.zip")
+resource "aws_lambda_function" "lambda_function" {
+  filename      = "${path.module}/source/lambda_function.zip"
+  function_name = "notify_aws_cost"  # 任意の関数名に
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.10"
+  source_code_hash = filebase64sha256("${path.module}/source/lambda_function.zip")
 
-#   environment {
-#     variables = {
-#       SNS_TOPIC_ARN = aws_sns_topic.lambda_sns_topic.arn
-#     }
-#   }
-# }
+  environment {
+    variables = {
+      SNS_TOPIC_ARN = aws_sns_topic.lambda_sns_topic.arn
+    }
+  }
+}
 
 # resource "aws_iam_role" "lambda_exec_role" {
 #   name = "lambda_exec_role"
